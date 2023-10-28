ec366b6f249880848f86890a1c5fb4ac5be3fe89
Harley Davidson Saring <harleysaring123@gmail.com>
2023-10-28 23:37:39 +0100

    new pages added

---
+++ ShugyopediaApp.Site/Controllers/TrainingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShugyopediaApp.Site.Controllers
+{
+    public class TrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
