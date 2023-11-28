df0a01a30460dd2178a2fd578e1af37ee88a1eff
white-dough <dwight.eyac20@gmail.com>
2023-11-28 19:15:48 +0800

    ratings

--- ShugyopediaApp.Services/ServiceModels/RatingViewModel.cs
+++ ShugyopediaApp.Services/ServiceModels/RatingViewModel.cs
@@ -9,6 +9,7 @@ namespace ShugyopediaApp.Services.ServiceModels
     public class RatingViewModel
     {
         public string RatingReview { get; set; }
+        public string TrainingName { get; set; }
         public double Rate { get; set; }
         public string RaterName { get; set; }
         public string RaterEmail { get; set; }
