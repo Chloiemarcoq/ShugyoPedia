da789bca3ad1911e643cad77f2915ff1584af830
Moses <mosesfat@gmail.com>
2023-11-30 19:55:15 +0800

    delete not yet done

--- ShugyopediaApp.Services/ServiceModels/RatingViewModel.cs
+++ ShugyopediaApp.Services/ServiceModels/RatingViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShugyopediaApp.Data.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,8 @@ namespace ShugyopediaApp.Services.ServiceModels
 {
     public class RatingViewModel
     {
-        public string RatingReview { get; set; }
         public string TrainingName { get; set; }
+        public string RatingReview { get; set; }
         public double Rate { get; set; }
         public string RaterName { get; set; }
         public string RaterEmail { get; set; }
