c1cf61101afbe4fddc3fa39bbf5d463e23666625
Harley Davidson Saring <harleysaring123@gmail.com>
2023-12-02 08:35:16 +0800

    added live search

--- ShugyopediaApp.Site/wwwroot/js/site.js
+++ ShugyopediaApp.Site/wwwroot/js/site.js
@@ -1,4 +1,37 @@
-﻿var sidebar = document.getElementById("sidebar");
+﻿/*SEARCH TRAINING*/
+document.getElementById('search-training').addEventListener('input', function () {
+    var searchQuery = this.value.toLowerCase();
+    document.querySelectorAll('.card').forEach(function (card) {
+        var find_training = card.querySelector('.topic-word').textContent.toLowerCase();
+        card.style.display = find_training.includes(searchQuery) ? 'block' : 'none';
+    });
+});
+
+/*SEARCH CATEGORY*/
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('search-category');
+    const list = document.getElementById('category-list');
+    const items = list.getElementsByTagName('li');
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        for (let i = 0; i < items.length; i++) {
+            const text = items[i].innerText.toLowerCase();
+
+            if (text.includes(searchTerm)) {
+                items[i].style.display = 'block';
+            } else {
+                items[i].style.display = 'none';
+            }
+        }
+    });
+});
+
+
+
+
+var sidebar = document.getElementById("sidebar");
 var read_more = document.getElementById("read-more");
 
 sidebar.addEventListener("mouseenter", () => {
