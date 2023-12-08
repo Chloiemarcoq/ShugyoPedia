37e410f186542748f6867259ba77abeb2bcbc268
Harley Davidson Saring <harleysaring123@gmail.com>
2023-12-08 18:43:25 +0800

    minor changes

--- ShugyopediaApp.Site/wwwroot/js/site.js
+++ ShugyopediaApp.Site/wwwroot/js/site.js
@@ -27,13 +27,13 @@ document.addEventListener('DOMContentLoaded', function () {
 var sidebar = document.getElementById("sidebar");
 var read_more = document.getElementById("read-more");
 
-sidebar.addEventListener("mouseenter", () => {
+/*sidebar.addEventListener("mouseenter", () => {
     document.getElementById("nav-link").classList.remove("active");
 })
 
 sidebar.addEventListener("mouseleave", () => {
     document.getElementById("nav-link").classList.add("active");
-})
+})*/
 
 read_more.addEventListener("onclick", () => {
     document.getElementById("nav-link").classList.add("active");
